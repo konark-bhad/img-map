a66b6bc6d6ebe88b90e4a3755d9c0b84258c7a0e
konark-bhad <53810525+konark-bhad@users.noreply.github.com>
2020-09-06 17:46:38 +0530

    Updates

---
+++ precache-manifest.e82f28b611c8e70229925a2350a78105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743fb2b634058081ecb40c81f7bcbed3",
+    "url": "/img-map/index.html"
+  },
+  {
+    "revision": "12996d009f96bf99326f",
+    "url": "/img-map/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9e6a08e410512a3f449c",
+    "url": "/img-map/static/js/2.5889db69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/img-map/static/js/2.5889db69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12996d009f96bf99326f",
+    "url": "/img-map/static/js/main.d94eac62.chunk.js"
+  },
+  {
+    "revision": "ecd59075a23d15d2c7b1",
+    "url": "/img-map/static/js/runtime-main.c8e72664.js"
+  }
+]);
